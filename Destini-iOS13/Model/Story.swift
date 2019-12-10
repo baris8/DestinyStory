38926253b3cd1286faab2725521a58247effc628
uectas <baris.uectas@uni-duesseldorf.de>
2019-12-10 01:15:16 +0100

    finished project

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice2: String
+    let choice1destination: Int
+    let choice2destination: Int
+    
+    
+    init(title: String, choice1: String, choice1dest: Int, choice2: String, choice2dest: Int) {
+        self.title = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+        self.choice1destination = choice1dest
+        self.choice2destination = choice2dest
+    }
+}
